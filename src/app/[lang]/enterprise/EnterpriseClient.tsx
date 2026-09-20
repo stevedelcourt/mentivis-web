16ef4206ded75f1c5e6cdb55bc4263ea93ea0ab0
Steven Delcourt <Steven.delcourt@mentivis.com>
2026-09-20 12:38:28 +0200

    feat: enterprise hero men-enterprise.avif (48KB 2000x1143) replaces hero-2

--- src/app/[lang]/enterprise/EnterpriseClient.tsx
+++ src/app/[lang]/enterprise/EnterpriseClient.tsx
@@ -26,12 +26,12 @@ export default function EnterpriseClient() {
         description: e.heroLead,
         provider: { "@id": `${SITE.baseUrl}/#organization` },
         url: `${SITE.baseUrl}/${lang}/enterprise`,
-        image: `${SITE.baseUrl}/images/heroes/hero-2.avif`,
+        image: `${SITE.baseUrl}/images/heroes/men-enterprise.avif`,
         areaServed: { "@type": "Country", name: "France" },
         inLanguage: lang === "fr" ? "fr-FR" : "en-US",
       }} />
       <ImageHero
-        image="/images/heroes/hero-2.avif"
+        image="/images/heroes/men-enterprise.avif"
         eyebrow={e.eyebrow}
         title={<span style={{ color: "white" }}>{e.heroTitle[0]} <em style={{ color: "white" }}>{e.heroTitle[1]}</em></span>}
         lead={e.heroLead}
